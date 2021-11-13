dd997d43d7927908e23816141092496511d3dbe5
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-11-13 16:41:30 +0100

    first init

---
+++ validation.js
@@ -0,0 +1,22 @@
+
+var email = document.getElementById('email');
+var password = document.getElementById('pass');
+var submit = document.getElementById('submit');
+
+
+submit.addEventListener("click", () => {
+    if (Validate(email.value) & password.value != '') {
+        
+        window.alert("Logging successed!");
+        window.location.href = '/accueil.html';
+        
+    } else {
+        window.alert("Please enter a valid email and password");
+    }
+});
+
+function Validate(value) {
+    //using regex expretion for validation
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(value).toLowerCase());
+}
