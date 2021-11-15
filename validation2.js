9cbd66dc5955662ef5f088c9cd671fe242a13a70
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-11-15 18:44:08 +0100

    well its not

--- validation2.js
+++ validation2.js
@@ -1,32 +1,43 @@
 var inputs = document.getElementsByClassName('inputs')
 var submit = document.getElementById('submit');
+
 var pass;
 
 //user info
 user = [];
 
+
+
+
 function formOnSubmit() {
+    
+    
 
     //adding user data to the user array
     for (let i = 0; i < inputs.length; i++) {
         user[i] = inputs[i].value;
     }
 
-    localStorage.setObj("userInfo", user);
+    
 
     ValidateAll(inputs);
+
+
     if (pass) {
+        localStorage.setObj("userInfo", user);
         window.alert("le compte a ete cree avec succes!");
         window.location.href = '/accueil.html';
     } else {
         window.alert("Please Fill all fields...");
     }
+    
 };
 
 function ValidateAll(_array) {
     for (let i = 0; i < _array.length; i++) {
-        if (_array[i].value === "") {
+        if (_array[i].value === '') {
             pass = false;
+            break;
         }else{
             pass = true;
         }
