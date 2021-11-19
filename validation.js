cfb0c0d887a21d15453b26c0869cb7a810d85eb5
Raid Teyar <63502859+raid-teyar@users.noreply.github.com>
2021-11-19 15:50:44 +0100

    fixed login page validation

--- validation.js
+++ validation.js
@@ -2,12 +2,15 @@
 var email = document.getElementById('email');
 var password = document.getElementById('pass');
 var submit = document.getElementById('submit');
+var user = [];
 
 submit.addEventListener("click", () => {
     if (Validate(email.value) & password.value != '') {
-        
+        user[0] = email.value;
+        user[1] = password.value;
         window.alert("Logging successed!");
         window.location.href = 'accueil.html';
+        localStorage.setObj("userInfo", user);
         
     } else {
         window.alert("Please enter a valid email and password");
@@ -20,3 +23,7 @@ function Validate(value) {
     return re.test(String(value).toLowerCase());
 }
 
+//declaring functions to store info in local storage as JSON since local storage support only strings and not arrays
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
